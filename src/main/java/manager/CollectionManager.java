758c8048ecc7526cf96b0c0de17cc0e2a11ac45b
Jacob Michael <tjm4au@gmail.com>
2023-04-04 21:03:02 -0500

    Implemented save() for CollectionManager

--- src/main/java/manager/CollectionManager.java
+++ src/main/java/manager/CollectionManager.java
@@ -5,7 +5,18 @@ import main.java.io.Savable;
 import main.java.io.XMLParser;
 import main.java.model.Collection;
 import main.java.model.Game;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -197,7 +208,56 @@ public class CollectionManager implements Loadable, Savable {
     }
 
     @Override
-    public void save() throws IOException {
+    public void save() {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 
+            // Create the root element "collections"
+            Document doc = dBuilder.newDocument();
+            Element rootElement = doc.createElement("collections");
+            doc.appendChild(rootElement);
+
+            // For each user ID, create a new "user" element with child "collection" elements
+            for (Map.Entry<String, List<Collection>> userEntry : collectionMap.entrySet()) {
+                String userId = userEntry.getKey();
+                List<Collection> userCollections = userEntry.getValue();
+
+                Element userElement = doc.createElement("user");
+                userElement.setAttribute("id", userId);
+
+                for (Collection collection : userCollections) {
+                    Element collectionElement = doc.createElement("collection");
+                    collectionElement.setAttribute("id", collection.getId());
+                    collectionElement.setAttribute("name", collection.getName());
+
+                    Element descriptionElement = doc.createElement("description");
+                    descriptionElement.appendChild(doc.createTextNode(collection.getDescription()));
+                    collectionElement.appendChild(descriptionElement);
+
+                    for (String gameId : collection.getGames()) {
+                        Element gameElement = doc.createElement("game");
+                        gameElement.appendChild(doc.createTextNode(gameId));
+                        collectionElement.appendChild(gameElement);
+                    }
+
+                    userElement.appendChild(collectionElement);
+                }
+
+                rootElement.appendChild(userElement);
+            }
+
+            // Use a Transformer to write the Document to an XML file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File(COLLECTIONS_FILE_PATH));
+            transformer.transform(source, result);
+
+        } catch (TransformerException | ParserConfigurationException e) {
+            e.printStackTrace();
+            System.out.println("Failed to save collections.");
+        }
     }
+
 }
