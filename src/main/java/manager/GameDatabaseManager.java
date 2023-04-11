8b53f59ea844ad55a913a827e821044387475ee9
Thomas Michael <thomasjmichael@outlook.com>
2023-04-11 16:19:56 -0500

    Added documentation for the GameDatabaseManager

--- src/main/java/manager/GameDatabaseManager.java
+++ src/main/java/manager/GameDatabaseManager.java
@@ -1,3 +1,9 @@
+/**
+ * GameDatabaseManager is a singleton class that manages a map of game details, loaded from an XML file using the
+ * Loadable interface. It provides static methods to access the game details map, retrieve details for a specific game,
+ * and load the game file. The game file path is read from the ConfigManager properties file.
+ */
+
 package main.java.manager;
 
 import main.java.io.Loadable;
@@ -15,9 +21,16 @@ public class GameDatabaseManager implements Loadable {
     private static String GAME_FILE_PATH;
     private static GameDatabaseManager instance = null;
     private static Map<String, GameDetails> gameDetailsMap;
-
+    /**
+     * Private constructor to prevent instantiation.
+     */
     private GameDatabaseManager(){}
 
+    /**
+     * Singleton pattern that loads the game file.
+     *
+     * @return A new instance of the GameDatabaseManager or the existing instance.
+     */
     synchronized public static GameDatabaseManager getInstance(){
         if (instance == null){
             instance = new GameDatabaseManager();
@@ -30,11 +43,20 @@ public class GameDatabaseManager implements Loadable {
         }
         return instance;
     }
-
+    /**
+     * Gets the game details map for a Game object.
+     *
+     * @return The game details map.
+     */
     public static GameDetails getGameDetails(Game game){
         return gameDetailsMap.get(game.getId());
     }
-
+    /**
+     * Gets the game details map for a game ID.
+     *
+     * @param id The string ID of the game.
+     * @return The game details map.
+     */
     public static GameDetails getGameDetailsByID(String id){
         if (instance == null){
             GameDatabaseManager.getInstance();
@@ -46,13 +68,20 @@ public class GameDatabaseManager implements Loadable {
         return gameDetailsMap.get(id);
     }
 
+    /**
+     * Gets the game details map for all the games.
+     *
+     * @return The game details map.
+     */
     public static Map<String, GameDetails> getDetailsMap () {
         if (instance == null){
             getInstance();
         }
         return gameDetailsMap;
     }
-
+    /**
+     * Loads the game file using the Loadable interface.
+     */
     @Override
     public void load() {
         if (instance == null){
